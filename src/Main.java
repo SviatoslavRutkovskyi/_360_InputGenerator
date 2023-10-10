d2de6ec3f23398aa749c18cec6c6da945332fdf5
Someonelse <rutkovsky2003@gmail.com>
2023-10-10 09:50:31 -0700

    Fixed checkstyle

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+/**
+ * Authors: Sviatoslav Rutkovskyi, Minh Vu, Andrew Holmes.
+ * Date: 10/10/2023
+ * TCSS 360
+ */
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    /**
+     * Percentage multiplier for Math.random().
+     */
+    private static final int PERCENTAGE = 100;
+
+    /**
+     * Main method that prompts user and
+     * creates test code for Minesweeper Class.
+     *
+     * @param theArgs passed in argument
+     */
+    public static void main(final String[] theArgs) {
         System.out.println("Hello world!");
 
         Scanner sc = new Scanner(System.in);
@@ -11,7 +27,6 @@ public class Main {
             rand = sc.nextLine();
 
         } while (!rand.equals("no") && !rand.equals("yes"));
-
 
         if (rand.equals("no")) {
             System.out.println("Rows: ");
@@ -26,22 +41,24 @@ public class Main {
             System.out.println("# 0f boards: ");
             int boardNum = sc.nextInt();
             for (int i = 0; i < boardNum; i++) {
-                int rows = (int) (Math.random() * 100);
-                int col = (int) (Math.random() * 100);
-                double percentage = Math.random() * 100;
+                int rows = (int) (Math.random() * PERCENTAGE);
+                int col = (int) (Math.random() * PERCENTAGE);
+                double percentage = Math.random() * PERCENTAGE;
                 generateBoard(rows, col, percentage);
             }
         }
 
 
     }
-    private static void generateBoard(int theRows, int theCol, double thePercentage){
+
+    private static void generateBoard(
+            final int theRows, final int theCol, final double thePercentage) {
         char[][] minefield = new char[theRows][theCol];
 
 
         for (int i = 0; i < theRows; i++) {
             for (int j = 0; j < theCol; j++) {
-                if (thePercentage > Math.random() * 100) {
+                if (thePercentage > Math.random() * PERCENTAGE) {
                     minefield[i][j] = '*';
                 } else {
                     minefield[i][j] = '.';
